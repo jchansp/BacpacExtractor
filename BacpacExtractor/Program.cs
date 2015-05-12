2afda20c24976329c97bf240ee90201a5adf79bf
jchansp <jciansp@hotmail.com>
2015-05-12 10:53:53 +0200

    DownloadFile method created

--- BacpacExtractor/Program.cs
+++ BacpacExtractor/Program.cs
@@ -5,20 +5,25 @@ namespace BacpacExtractor
 {
     internal class Program
     {
+        private const string RemoteUri = "http://eduapp.edumod2.net/Subscripciones/DatosColegios/DescargarBackup?nombreArchivo=";
+        private const string FileName = "EX-WUS-Educamos_100095-20150512021703.bacpac";
+
         private static void Main(string[] args)
         {
-            const string remoteUri =
-                "http://eduapp.edumod2.net/Subscripciones/DatosColegios/DescargarBackup?nombreArchivo=";
-            const string fileName = "EX-WUS-Educamos_100095-20150512021703.bacpac";
+            DownloadFile();
+        }
+
+        private static void DownloadFile()
+        {
             string myStringWebResource = null;
             // Create a new WebClient instance.
             var myWebClient = new WebClient();
             // Concatenate the domain with the Web resource filename.
-            myStringWebResource = remoteUri + fileName;
-            Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, myStringWebResource);
+            myStringWebResource = RemoteUri + FileName;
+            Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", FileName, myStringWebResource);
             // Download the Web resource and save it into the current filesystem folder.
-            myWebClient.DownloadFile(myStringWebResource, fileName);
-            Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, myStringWebResource);
+            myWebClient.DownloadFile(myStringWebResource, FileName);
+            Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", FileName, myStringWebResource);
         }
     }
 }
